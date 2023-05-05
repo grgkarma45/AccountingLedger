d5cdd12425d385c3addffc39596bc955c405cd34
grgkarma45 <grgkarma45@gmail.com>
2023-05-05 18:44:48 -0400

    Project Completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,20 @@
-package org.example;
+import org.example.Ledger;
+
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
-
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         homescreen();
     }
 
     public static void homescreen() {
-
         Scanner scanner = new Scanner(System.in);
-        String input = null;
-        do{
+        String input;
+        do {
             //Display Homescreen
             System.out.println("Welcome to your accounting Ledger: ");
             System.out.println(("Main Menu"));
@@ -24,7 +24,7 @@ public class Main {
             System.out.println("[X] - Exit");
             // use switch method for the options in the Homescreen
             input = scanner.nextLine();
-            switch (input.toUpperCase()){
+            switch (input.toUpperCase()) {
                 case "D":
                     addDeposit();
                     break;
@@ -41,65 +41,62 @@ public class Main {
                 default:
                     System.out.println("Please enter a valid option and try again");
                     break;
-
-            }}while(!input.equalsIgnoreCase("x"));
-        //System.out.println("Have a Nice Day");
-
-
+            }
+        } while (!input.equalsIgnoreCase("x"));
     }
 
-    private static void showLedger() { // Calling showLedger() method from the ledger class
+    private static void showLedger() {
         Ledger.showLedger();
     }
 
-
-
-    private static void makePayment() {  // using Scanner to get user's input and storing it in the corresponding variable
+    private static void addDeposit() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter Date:(yyyy-MM-dd)");
         String date = scanner.nextLine();
         System.out.println("Enter Time: ( HH:mm:SS");
         String time = scanner.nextLine();
         System.out.println("Enter Description");
-        String description =scanner.nextLine();
+        String description = scanner.nextLine();
         System.out.println("Enter vendor");
-        String vendor = scanner.next();
+        String vendor = scanner.nextLine(); // use nextLine() to read the vendor name
         System.out.println("Enter amount");
-        double amount = scanner.nextDouble();//Storing  the amount as the Double.
+        double amount = scanner.nextDouble();
 
-        try{ // writing the information from the variables to the csv.file
+        try {
             FileWriter fileWriter = new FileWriter("transactions.csv", true);
-            //add | in between the variables
-            fileWriter.write( "\n" + date + "|" + time + "|" +description +"|"+ vendor + "|" + amount);
+            // use String.format() to format the amount with 2 decimal places
+            fileWriter.write(String.format("\n%s|%s|%s|%s|%.2f", date, time, description, vendor, amount));
             System.out.println("Deposit added successfully");
             fileWriter.close();
-        } catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Error inputting data!");
         }
     }
 
-    private static void addDeposit() { // using Scanner to get user's input and storing it in a corresponding variable
+
+    private static void makePayment() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter Date:(yyyy-MM-dd)");
         String date = scanner.nextLine();
         System.out.println("Enter Time: ( HH:mm:SS");
         String time = scanner.nextLine();
         System.out.println("Enter Description");
-        String description =scanner.nextLine();
+        String description = scanner.nextLine();
         System.out.println("Enter vendor");
-        String vendor = scanner.next();
+        String vendor = scanner.nextLine();
         System.out.println("Enter amount");
-        double amount = scanner.nextDouble();
+        double amount = scanner.nextDouble() *-1;
 
-        try{ // writing the information from the variables to the csv.file
+        DecimalFormat df = new DecimalFormat("#.##"); // format amount with 2 decimal places
+        String formattedAmount = df.format(amount);
+
+        try {
             FileWriter fileWriter = new FileWriter("transactions.csv", true);
-            //add | in between the variables
-            fileWriter.write( "\n" + date + "|" + time + "|" +description +"|"+ vendor + "|" + amount);
-            System.out.println("Deposit added successfully");
+            fileWriter.write("\n" + date + "|" + time + "|" + description + "|" + vendor + "|" + formattedAmount);
+            System.out.println("Payment made successfully");
             fileWriter.close();
-        } catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Error inputting data!");
         }
-
     }
 }
